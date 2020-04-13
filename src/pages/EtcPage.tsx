9df0cb63c7c26b5cfdd56d50274eba9260aab53e
hkkokily5 <hkkokily5@gmail.com>
2020-04-14 07:01:04 +0900

    Change Image

--- src/pages/EtcPage.tsx
+++ src/pages/EtcPage.tsx
@@ -6,11 +6,11 @@ const StudyZonePage = () => {
   return (
     <PageTemplate>
       <Interior
-        title="기 타"
+        title=""
         image="/assets/custom/rooms/etc.jpg"
-        category="공부 존"
-        usage="공부하는 곳"
-        allowance="8명"
+        category=""
+        usage=""
+        allowance=""
       />
     </PageTemplate>
   );
