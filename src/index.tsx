9f64454b8802ab7c1d806cb1cde3d66da9ccb882
kokily <kokily@naver.com>
2020-05-29 16:41:25 +0900

    RePlace

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import { render, hydrate } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error('Root Element is missing!');
-}
-
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>,
-    rootElement,
-  );
-} else {
-  render(
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>,
-    rootElement,
-  );
-}
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
